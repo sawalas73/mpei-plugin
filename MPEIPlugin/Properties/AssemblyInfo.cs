1cb32962073f317946f5b04448b89a7f38ad1e8c
damien.haynes <damien.haynes@ed295ee8-e027-11de-ab17-5d3be3536607>
2011-09-08 03:38:01 +0000

    Automated and Common versioning between MPEIHelper and MPEIPlugin projects. Changed versioning to v1.0.0 (mark next release as stable).

--- MPEIPlugin/Properties/AssemblyInfo.cs
+++ MPEIPlugin/Properties/AssemblyInfo.cs
@@ -23,19 +23,6 @@ using MediaPortal.Common.Utils;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("66130e87-42a1-441e-a670-cda3e50605e0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5.9")]
-[assembly: AssemblyFileVersion("0.0.5.9")]
-
 [assembly: CompatibleVersion("1.1.6.27644")]
 [assembly: UsesSubsystem("MP.SkinEngine")]
 [assembly: UsesSubsystem("MP.Players.Video")]
