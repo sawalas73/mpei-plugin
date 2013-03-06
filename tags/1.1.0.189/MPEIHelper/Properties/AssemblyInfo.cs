f44557db127eb798c36caa7036378c8ef9e78575
damien.haynes <damien.haynes@ed295ee8-e027-11de-ab17-5d3be3536607>
2013-03-06 10:19:35 +0000

    Created tag for v1.1.0.189 release.

---
+++ tags/1.1.0.189/MPEIHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MPEIHelper")]
+[assembly: AssemblyDescription("Extensions Helper for MediaPortal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MediaPortal")]
+[assembly: AssemblyProduct("MPEIHelper")]
+[assembly: AssemblyCopyright("Copyright © MediaPortal 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c0a416b-7764-4356-9e92-8ece9d545e71")]
---
+++ tags/1.1.0.189/MPEIPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MediaPortal.Common.Utils;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MPEIPlugin")]
+[assembly: AssemblyDescription("Extensions plugin for MediaPortal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MediaPortal")]
+[assembly: AssemblyProduct("MPEIPlugin")]
+[assembly: AssemblyCopyright("Copyright © MediaPortal 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66130e87-42a1-441e-a670-cda3e50605e0")]
+
+[assembly: CompatibleVersion("1.2.100.0", "1.1.6.27644")]
+[assembly: UsesSubsystem("MP.SkinEngine")]
+[assembly: UsesSubsystem("MP.Players.Video")]
+[assembly: UsesSubsystem("MP.Players.Music")]
+[assembly: UsesSubsystem("MP.Config")]
